ba1311d482f5d674c62e3d487b7894aa46bbeca5
Student <student@Makerss-MacBook-Air.local>
2019-11-14 14:49:53 +0000

    step 12 and 13, raising errors

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,10 +3,12 @@ class DockingStation
   
     def release_bike
        Bike.new
+       return fail "no bikes available" unless @bike
     end
   
   def dock(bike)
-    @bike = bike
+    # @bike = bike
+    return fail "no space available" unless @bike
   end
   
   # def bike       commented out due to attr_reader
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -16,10 +16,24 @@ it {is_expected.to respond_to(:bike)}
 
 #it {expect(dock(bike)).to eq true }
 
+describe '#release_bike' do
+  it "raises an error when there are no bikes available" do
+    expect { subject.release_bike }.to raise_error(RuntimeError, "no bikes available")
+  end
 end
 
+describe "#dock" do
+  it "raises an error when the docking station is at capacity" do
+    bike = Bike.new
+    expect { subject.dock(bike) }.to raise_error(RuntimeError, "no space available")
+  end
+end
 
+end
 
 describe Bike do
-  it {is_expected.to respond_to(:working?)}
+  # it {is_expected.to respond_to(:working?)}
+  
+
+
 end
